e822d2809d23cd25238a635a718ccc2bcb91087b
Riley Coad <rileyjcoad@gmail.com>
2023-08-10 20:47:42 -0500

    Adding menu icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import FlexTable from "./FlexTable";
+// import FlexTable from "./FlexTable";
 import Button from "./button";
 
 const App = () => {
@@ -9,14 +9,24 @@ const App = () => {
   };
   return (
     <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button class="sidebar-close">
+          <span class="material-symbols-outlined">menu</span>
+        </button>
+        <sidebar>
+          <div class="sidebar-header"></div>
+        </sidebar>
       </header>
-      <div className="menu">
-        <Button label="i'm a button" onClick={handleButton} />
-        <Button label="i'm a button" onClick={handleButton} />
-        <Button label="i'm a button" onClick={handleButton} />
-      </div>
+      <menu>
+        <Button label="pottery" onClick={handleButton} />
+        <Button label="photography" onClick={handleButton} />
+        <Button label="projects" onClick={handleButton} />
+      </menu>
     </div>
   );
 };
