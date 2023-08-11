8ebc82e5deaf8b4a69413856582373955f8983b6
Riley Coad <rileyjcoad@gmail.com>
2023-08-11 10:25:03 -0500

    Changing around home screen and renaming files

--- src/Home.js
+++ src/Home.js
@@ -1,37 +1,37 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./Home.css";
 // import FlexTable from "./FlexTable";
 import Button from "./button";
 
-const App = () => {
+const Home = () => {
   const handleButton = () => {
     console.log("Button clicked uwu");
   };
   return (
-    <div className="App">
+    <div className="Home">
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
       />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
         <button class="sidebar-close">
           <span class="material-symbols-outlined">menu</span>
         </button>
         <sidebar>
           <div class="sidebar-header"></div>
         </sidebar>
+        <menu>
+          <Button label="pottery" onClick={handleButton} />
+          <Button label="photography" onClick={handleButton} />
+          <Button label="projects" onClick={handleButton} />
+        </menu>
       </header>
-      <menu>
-        <Button label="pottery" onClick={handleButton} />
-        <Button label="photography" onClick={handleButton} />
-        <Button label="projects" onClick={handleButton} />
-      </menu>
     </div>
   );
 };
 
-export default App;
+export default Home;
 
 /* <h1>Flex Table Example</h1>
 <FlexTable /> */
